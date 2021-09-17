814eab04f5646b6f2a6782ef70fcfb1c4db1b6ec
Lin  Kuan-Chun <linkuanc@ug226.eecg.toronto.edu>
2021-09-17 13:49:52 -0400

    add intro to the certificates I take in helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
 print("Hello World" + "Jean" + " 4 years at U of T")
 print("Nick here adding a line")
 print("My major is computer engineering")
+print("I'm taking certificates in AI and Engineering Business")
